23cc2a98299b3565519d8651bdc8533d0bf601f3
Evan Cowden <evan.cowden@wwt.com>
2013-03-09 14:08:34 -0600

    Cleans app.js of extraneous comments and code

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,8 @@
 (function (angular) {
     'use strict';
 
-    var ngModule = angular.module('blind-satellite', [
+    angular.module('blind-satellite', [
         'blind-satellite.indexCtrl'
     ]);
-//    ngModule.config(['$routeProvider', function ($routeProvider) {
-//        $routeProvider.when('/', {});
-//        $routeProvider.otherwise({redirectTo: '/'});
-//    }]);
+
 }(window.angular));
